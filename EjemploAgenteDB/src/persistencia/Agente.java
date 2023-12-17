44441ecf8ece5385d01ba7d1a124e05f29e8f97e
SSCA2234 <153659876+SSCA2234@users.noreply.github.com>
2023-12-17 15:18:22 +0100

    Update Agente.java

--- EjemploAgenteDB/src/persistencia/Agente.java
+++ EjemploAgenteDB/src/persistencia/Agente.java
@@ -14,9 +14,9 @@ public class Agente {
 	// Conexion con la base de datos
 	protected static Connection mBD;
 	// Identificador ODBC de la base de datos
-	private static String url = "jdbc:mysql://localhost:3307/practicabd?user=alumno&password=alumno";
+	private static String url = "jdbc:mysql://localhost:3306/usuarios?user=root&password=pass";
 	// Driven para conectar con bases de datos MySQL
-	private static String driver = "com.mysql.jdbc.Driver";
+	private static String driver = "com.mysql.cj.jdbc.Driver";
 
 	// Constructor
 	private Agente() throws Exception {
@@ -24,10 +24,7 @@ public class Agente {
 
 	}
 
-	// Implementacion del patron singleton
-	// Este patron de dise駉 permite implementar clases de las cuales
-	// solo existir una instancia
-	// http://es.wikipedia.org/wiki/Singleton
+	/*Asignamos el cosntructor*/
 	public static Agente getAgente() throws Exception {
 		if (mInstancia == null) {
 			mInstancia = new Agente();
@@ -75,10 +72,12 @@ public class Agente {
 		return res;
 	}
 
+	/*M茅todo ppara seleccionar informaci贸n en la base de datos, devolviendo un vector*/
 	public Vector<Object> select(String SQL) throws SQLException, Exception {
+
 		/*
 		 * Metodo para realizar una busqueda o seleccion de informacion enla base de
-		 * datos El m巘odo select develve un vector de vectores, donde cada uno de los
+		 * datos El m todo select develve un vector de vectores, donde cada uno de los
 		 * vectores que contiene el vector principal representa los registros que se
 		 * recuperan de la base de datos.
 		 */
@@ -98,4 +97,93 @@ public class Agente {
 		return vectoradevolver;
 
 	}
+	
+	/*M茅todo ppara seleccionar la consulta seleccionadoa mediante el selector en la base de datos, devolviendo un vector*/
+	public Vector<Object> selectConsultas(String SQL) throws SQLException, Exception {
+		/*
+		 * Metodo para realizar una busqueda o seleccion de informacion enla base de
+		 * datos El m todo select develve un vector de vectores, donde cada uno de los
+		 * vectores que contiene el vector principal representa los registros que se
+		 * recuperan de la base de datos.
+		 */
+
+		Vector<Object> vectoradevolver = new Vector<Object>();
+		conectar();
+		Statement stmt = mBD.createStatement();
+		ResultSet res = stmt.executeQuery(SQL);
+		while (res.next()) {
+			Vector<Object> v = new Vector<Object>();
+			v.add(res.getObject(1));
+			vectoradevolver.add(v);
+		}
+		stmt.close();
+		desconectar();
+		return vectoradevolver;
+
+	}
+	
+	/*M茅todo ppara seleccionar informaci贸n en la base de datos, devolviendo un vector*/
+	public Vector<Object> select2(String SQL, Vector<Object> params) throws SQLException, Exception {
+	    Vector<Object> vectoradevolver = new Vector<Object>();
+	    conectar();
+	    PreparedStatement pstmt = mBD.prepareStatement(SQL);
+
+	    for (int i = 0; i < params.size(); i++) {
+	        pstmt.setObject(i + 1, params.elementAt(i));
+	    }
+
+	    ResultSet res = pstmt.executeQuery();
+
+	    while (res.next()) {
+	        Vector<Object> v = new Vector<Object>();
+	        for (int i = 1; i <= res.getMetaData().getColumnCount(); i++) {
+	            Object valor = res.getObject(i);
+	            if (valor instanceof Boolean) {
+	                // Convertir Boolean a Integer
+	                v.add(((Boolean) valor) ? 1 : 0);
+	            } else {
+	                // Mantener otros tipos de datos sin cambios
+	                v.add(valor);
+	            }
+	        }
+	        vectoradevolver.add(v);
+	    }
+
+	    pstmt.close();
+	    desconectar();
+	    return vectoradevolver;
+	}
+	
+	// Metodo para realizar una insercion en la base de datos, mediante un vector de objetos, para mejorar su preci贸n y sencillez en el c贸digo
+	public int insert2(String SQL, Vector<Object> params) throws SQLException, Exception {
+	    conectar();
+	    PreparedStatement stmt = mBD.prepareStatement(SQL);
+
+	    // Configurar los par谩metros de la consulta preparada
+	    for (int i = 0; i < params.size(); i++) {
+	        stmt.setObject(i + 1, params.elementAt(i));
+	    }
+
+	    int res = stmt.executeUpdate();
+	    stmt.close();
+	    desconectar();
+	    return res;
+	}
+	
+	// Metodo para realizar una actualizaci贸n en la base de datos, mediante un vector de objetos, para mejorar su preci贸n y sencillez en el c贸digo
+	public int update2(String SQL, Vector<Object> params) throws SQLException, Exception {
+	    conectar();
+	    PreparedStatement stmt = mBD.prepareStatement(SQL);
+
+	    // Configurar los par谩metros de la consulta preparada
+	    for (int i = 0; i < params.size(); i++) {
+	        stmt.setObject(i + 1, params.elementAt(i));
+	    }
+
+	    int res = stmt.executeUpdate();
+	    stmt.close();
+	    desconectar();
+	    return res;
+	}
+
 }
