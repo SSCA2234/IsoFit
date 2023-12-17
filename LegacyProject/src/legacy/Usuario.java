5c8a14d59c15c49c49dc0053ab8f782053f5aaff
SSCA2234 <153659876+SSCA2234@users.noreply.github.com>
2023-12-17 15:17:42 +0100

    Update Usuario.java

--- LegacyProject/src/legacy/Usuario.java
+++ LegacyProject/src/legacy/Usuario.java
@@ -1,79 +1,138 @@
 package legacy;
 
-import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Vector;
 
-import org.apache.derby.jdbc.EmbeddedDriver;
+import javax.swing.JTextField;
+
+import persistencia.Agente;
 
 public class Usuario {
 	
 	public String mLogin;
-	public String mPassword;
+    public String mPassword;
+    public boolean esEspecialista;
 	
 	
-	//Constructor para la creaci梟 de un objeto Usuario vacio
-	public Usuario(){
-		this.mLogin = null;
-		this.mPassword = null;
-	}
+	//Constructor para la creaci n de un objeto Usuario vacio
+	public Usuario(String login, String password, boolean esEspecialista) {
+        this.mLogin = login;
+        this.mPassword = password;
+        this.esEspecialista = esEspecialista;
+    }
 	
-	//Constructor para la creaci梟 de un Usuario
-	public Usuario(String login, String password){
-		this.mLogin = login;
-		this.mPassword = password;
-	}
+	//Constructor para la creaci贸n de un Usuario
+	public Usuario(String login, String password) {
+        this.mLogin = login;
+        this.mPassword = password;
+        this.esEspecialista = false; // Por defecto, no es especialista
+    }
 	
-	//Selecci梟 de un usuario de la base de datos a partir del login y el password
-	@SuppressWarnings("unchecked")
+	//Selecci贸n de un usuario de la base de datos a partir del login y el password
 	public static Usuario read(String login, String password) throws Exception{
 		String l,g;
 		Usuario u = null;
 		Vector<Object> aux = null;
-		Driver derbyEmbeddedDriver = new EmbeddedDriver();
-		DriverManager.registerDriver(derbyEmbeddedDriver);
-		Connection mBD = DriverManager.getConnection(""+BDConstantes.DRIVER+":"+BDConstantes.DBNAME+";create=false", BDConstantes.DBUSER, BDConstantes.DBPASS);
-		String SQL_Consulta = "SELECT login, pass FROM Usuario WHERE login = '"+login+"' AND pass = '"+password+"'";
+
+		String SQL_Consulta = "SELECT email, password FROM login WHERE email = '"+login+"' AND password = '"+password+"'";
+		aux = Agente.getAgente().select(SQL_Consulta);
 		Vector<Object> vectoradevolver=new Vector<Object>();
-		Statement stmt = mBD.createStatement();
-		ResultSet res=stmt.executeQuery(SQL_Consulta);
-		while (res.next()) {
-			aux=new Vector<Object>();
-			aux.add(res.getObject(1));
-			aux.add(res.getObject(2));
-			vectoradevolver.add(aux);
-		}
-    	stmt.close();
-    	mBD.close();
-		aux = new Vector<Object>();
-		if (vectoradevolver.size() == 1){
-			aux = (Vector<Object>) vectoradevolver.elementAt(0);
-			u = new Usuario((String) aux.elementAt(0), (String) aux.elementAt(1));
+
+		Vector aux1 = new Vector <Object>();
+		if (aux.size() == 1){
+			aux1 = (Vector<Object>) aux.elementAt(0);
+			u = new Usuario((String) aux1.elementAt(0), (String) aux1.elementAt(1));
 		}
 		return u;
 	}
 	
-	//Inserci梟 de un nuevo usuario en la base de datos
-	public int insert() throws Exception{
-		Driver derbyEmbeddedDriver = new EmbeddedDriver();
-		DriverManager.registerDriver(derbyEmbeddedDriver);
-		Connection mBD = DriverManager.getConnection(""+BDConstantes.DRIVER+":"+BDConstantes.DBNAME+";create=false", BDConstantes.DBUSER, BDConstantes.DBPASS);
-		PreparedStatement stmt = mBD.prepareStatement("INSERT INTO Usuario VALUES('"+this.mLogin+"','"+this.mPassword+"')");
-    	int res=stmt.executeUpdate();
-    	stmt.close();
-    	mBD.close();
-		return res;
-	}
+	//Inserci贸n de un nuevo usuario en la base de datos
+	public int insert() throws Exception {
+        int res = Agente.getAgente().insert("INSERT INTO login VALUES('" + mLogin + "','" + mPassword + "', " + (esEspecialista ? 1 : 0) + ")");
+        return res;
+    }
 	
-	public int update () throws Exception{
-		//por ahora no nos ha hecho falta actualizar nada...
-		return 0;
+	/*Agregar una nueva consulta a nuestra base de datos a traves de una consulta y un vector de objetos*/
+	public int agregarConsulta(String consulta, String respuesta, String email) {
+	    try {
+	        // L贸gica para agregar la consulta a la base de datos, similar a tu m茅todo original
+	        String SQL_Insercion = "INSERT INTO consultas_deportivas (consulta, respuesta, email) VALUES (?, ?, ?)";
+	        Vector<Object> params = new Vector<>();
+	        params.add(consulta);
+	        params.add(respuesta);
+	        params.add(email);
+	        return Agente.getAgente().insert2(SQL_Insercion, params);
+	    } catch (Exception e) {
+	        e.printStackTrace();//Si no es posible realizar dicho m茅todo, que imprima donde y cuales son los distintos errores
+	        return -1; //El valor -1, al ser negativo, nos indica que es un error
+	    }
 	}
+	/*Agrega una respuesta  a nuestra base de datos, mediante un vector de objetos y una consulta de my sql*/
+	public int agregarRespuesta(String respuesta, String consulta,String email) throws Exception {
+
+	        try {
+	        String SQL_Actualizacion = "UPDATE consultas_deportivas SET Consulta = ? WHERE Respuesta = ? AND email = ?";
+	        Vector<Object> params = new Vector<>();
+	        params.add(consulta);
+	        params.add(respuesta);
+	        params.add(email);
+
+	        return Agente.getAgente().update2(SQL_Actualizacion, params);
+	        }catch(Exception e){
+	        e.printStackTrace();//Si no es posible realizar dicho m茅todo, que imprima donde y cuales son los distintos errores
+	        return -1;//El valor -1, al ser negativo, nos indica que es un error
+	        }
+	    }
 	
 
-	private String DBPORT="3308";
+	/*Obtiene las disitintas consultas de nuestra base de datose, recorriendo toda la base de datos*/
+	public List<String> getConsultas() throws Exception {
+	    String SQL_Consulta = "SELECT consulta FROM consultas_deportivas";
+	    Vector<Object> v = Agente.getAgente().selectConsultas(SQL_Consulta);
+	    List<String> lista = new ArrayList<>();
+
+	    for (Object consultaObj : v) {
+	        Vector<Object> consulta = (Vector<Object>) consultaObj;
+	        String pregunta = (String) consulta.elementAt(0);
+	        lista.add(pregunta);
+	    }
+	    return lista;
+	}
+
+     /*Nos indica si es o no un especialista, leyendo la columna correspondiente donde se alamcenan dichos valores*/
+    public boolean isEspecialista(String login) throws Exception {
+        String SQL_Consulta = "SELECT es_especialista FROM login WHERE email = ?";
+        Vector<Object> params = new Vector<>();
+        params.add(login);
+
+        Vector<Object> resultado = Agente.getAgente().select2(SQL_Consulta, params);
+        /*Si el ressultado es 1, entonces da lugar a que es especialista*/
+        if (resultado.size() == 1) {
+            Vector<Object> fila = (Vector<Object>) resultado.elementAt(0);
+            Object esEspecialistaObj = fila.elementAt(0);
+            if (esEspecialistaObj instanceof Integer) {
+                Integer esEspecialista = (Integer) esEspecialistaObj;
+                return esEspecialista == 1;
+            }
+        }
+
+        return false;
+    }
+    
+    /*Obtendgo las disitntas consultas para mostrarlas al usurio y mueda responder las consultas realizadas por los usuarios vip*/
+    public List<String> obtenerCuestionesSinRespuesta2() throws Exception {
+        List<String> cuestionesSinRespuesta = new ArrayList<>();
+
+        List<String> consultas = this.getConsultas();
+        return consultas;
+    }
+
+    /*obtengo el mail del usuario logeado*/
+    public String getEmail() {
+		
+		return this.mLogin;
+	}
+
+
 }
